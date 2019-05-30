d67d12e1966db23c6fb54fd68367460ca5ecc651
tanson <fiafa5000@163.com>
2019-05-30 14:10:57 +0800

    add

--- src/main/java/com/ewing/order/busi/ball/dao/ReportDao.java
+++ src/main/java/com/ewing/order/busi/ball/dao/ReportDao.java
@@ -84,7 +84,7 @@ public class ReportDao {
 
 	public List<BetDetailDto> findBetDetail(String account, String date) {
 		String sql = "SELECT account,match_status as matchStatus, CAST(r.ioratio*r.gold AS DECIMAL(10,1)) as wingold,date_format(r.create_time, '%Y%m%d%H') as createTime,r.gold,r.total,r.type,r.spread,r.result,r.n_result,r.league,r.team_c,r.team_h";
-		sql += " FROM bet_log_result r WHERE 1=1 AND CODE=560 ";
+		sql += " FROM bet_log_result r WHERE 1=1 AND CODE='560' ";
 		if (StringUtils.isNotEmpty(account)) {
 			sql += "and account ='" + account + "'  ";
 		}
@@ -101,7 +101,7 @@ public class ReportDao {
 	
 	public List<BetDetailDto> findSucBetDetail(String account, String startDate,Integer num) {
 		String sql = "SELECT account,match_status as matchStatus, CAST(r.ioratio*r.gold AS DECIMAL(10,1)) as wingold,date_format(r.create_time, '%Y%m%d%H') as createTime,r.gold,r.total,r.type,r.spread,r.result,r.n_result,r.league,r.team_c,r.team_h";
-		sql += " FROM bet_log_result r WHERE 1=1 AND CODE=560 and (errormsg IS NULL or gold>0) ";
+		sql += " FROM bet_log_result r WHERE 1=1 AND CODE='560'";
 		if (StringUtils.isNotEmpty(account)) {
 			sql += "and account ='" + account + "'  ";
 		}
